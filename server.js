25c9d9d4870ee5de72814543a6e562d989a03455
segwuonwu <segwuonwu@gmail.com>
2020-02-24 17:22:21 -0800

    made an api call and added some views

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios');
 const app = express();
 
 // Sets EJS as the view engine
@@ -13,12 +14,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 // Adds some logging to each request
-app.use(require('morgan')('dev'));
+//app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+    res.render('index');
 });
+
+app.get('/results', function(req, res) {
+    var qs = {
+        params: {
+            s: req.query.title,
+            apikey: process.env.OMDB_API_KEY
+        }
+    };
+
+    axios.get('https://www.omdbapi.com/', qs)
+        .then(function(response) {
+            var results = response.data.Search;
+            console.log(results);
+            res.render('results', { movies: results }); // movies is from the forEach loop in the result.ejs
+        }).catch(err => {
+            console.log(err);
+        }).finally(function() {
+            console.log("Made it to this function!");
+        })
+});
+
+
+app.get('/movies/:movie_id', function(req, res) {
+    res.send("Testing my GET /movies/:movie_id route")
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
